95731361a7ca7016e81fd68ab98c6aa3aeadba0a
Peter Palaga <ppalaga@redhat.com>
2020-01-05 18:50:57 +0100

    Add ahc-default.properties and ahc.properties to the native image

--- deployment/src/main/java/io/quarkus/ahc/deployment/AhcProcessor.java
+++ deployment/src/main/java/io/quarkus/ahc/deployment/AhcProcessor.java
@@ -2,6 +2,7 @@ package io.quarkus.ahc.deployment;
 
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 
 class AhcProcessor {
 
@@ -10,6 +11,13 @@ class AhcProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    NativeImageResourceBuildItem nativeImageResources() {
+        return new NativeImageResourceBuildItem(
+                "org/asynchttpclient/config/ahc-default.properties",
+                "org/asynchttpclient/config/ahc.properties");
     }
 
 }
