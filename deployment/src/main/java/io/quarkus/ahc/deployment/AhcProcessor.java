175233379985cfd96ca4df11e3e57397e6eb03c2
Peter Palaga <ppalaga@redhat.com>
2020-01-05 19:14:10 +0100

    Add the HTTPS support

--- deployment/src/main/java/io/quarkus/ahc/deployment/AhcProcessor.java
+++ deployment/src/main/java/io/quarkus/ahc/deployment/AhcProcessor.java
@@ -1,6 +1,7 @@
 package io.quarkus.ahc.deployment;
 
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 
@@ -18,6 +19,11 @@ class AhcProcessor {
         return new NativeImageResourceBuildItem(
                 "org/asynchttpclient/config/ahc-default.properties",
                 "org/asynchttpclient/config/ahc.properties");
+    }
+
+    @BuildStep
+    ExtensionSslNativeSupportBuildItem activateSslNativeSupport() {
+        return new ExtensionSslNativeSupportBuildItem(FEATURE);
     }
 
 }
